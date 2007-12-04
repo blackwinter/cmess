6f093b415f6adaf210ab0de8b7693b8f51e54179
Jens Wille <jens.wille@uni-koeln.de>
2007-12-04 16:22:19 +0000

    <cmess> Initial project commit.
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/cmess@116 d799ba13-25ec-4c21-81e7-7bafc1e0b839

---
+++ Rakefile
@@ -0,0 +1,29 @@
+# Utilizes global rake-tasks: alias rake="rake -r rake -R /path/to/rakelibdir"
+# (Base tasks at <http://prometheus.khi.uni-koeln.de/svn/scratch/rake-tasks/>)
+
+require 'lib/cmess/version'
+
+FILES = FileList['lib/**/*.rb'].to_a
+EXECS = FileList['bin/*'].to_a
+RDOCS = %w[README COPYING ChangeLog]
+OTHER = FileList['[A-Z]*', 'example/*', 'data/*'].to_a
+
+task(:doc_spec) {{
+  :title      => 'cmess Application documentation',
+  :rdoc_files => RDOCS + FILES
+}}
+
+task(:gem_spec) {{
+  :append_svnversion => false,
+  :name             => 'cmess',
+  :version          => CMess::VERSION,
+  :summary          => "Assist with handling messed up encodings " <<
+                       "(Currently includes the following tools: " <<
+                       "#{EXECS.map { |e| File.basename(e) }.join(', ')})",
+  :files            => FILES + EXECS + OTHER,
+  :require_path     => 'lib',
+  :bindir           => 'bin',
+  :executables      => EXECS,
+  :extra_rdoc_files => RDOCS,
+  :dependencies     => %w[]
+}}
---
+++ lib/cmess/version.rb
@@ -0,0 +1,55 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of cmess, the encoding tool-box.                                #
+#                                                                             #
+# Copyright (C) 2007 University of Cologne,                                   #
+#                    Albertus-Magnus-Platz,                                   #
+#                    50932 Cologne, Germany                                   #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# cmess is free software; you can redistribute it and/or modify it under the  #
+# terms of the GNU General Public License as published by the Free Software   #
+# Foundation; either version 3 of the License, or (at your option) any later  #
+# version.                                                                    #
+#                                                                             #
+# cmess is distributed in the hope that it will be useful, but WITHOUT ANY    #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       #
+# details.                                                                    #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with cmess. If not, see <http://www.gnu.org/licenses/>.                     #
+#                                                                             #
+###############################################################################
+#++
+
+module CMess
+
+  module Version
+
+    MAJOR = 0
+    MINOR = 0
+    TINY  = 1
+
+    class << self
+
+      # Returns array representation.
+      def to_a
+        [MAJOR, MINOR, TINY]
+      end
+
+      # Short-cut for version string.
+      def to_s
+        to_a.join('.')
+      end
+
+    end
+
+  end
+
+  VERSION = Version.to_s
+
+end
